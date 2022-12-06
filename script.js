e65dcfc4f79b748c2c4cfa06812e9a55fbb0c856
mine9607 <81383389+mine9607@users.noreply.github.com>
2022-12-06 11:49:29 -0500

    Added code to capitalize first letter of player or computer string

--- script.js
+++ script.js
@@ -21,30 +21,25 @@ function getPlayerChoice(){
 let playerSelection = getPlayerChoice()
 let computerSelection = getComputerChoice()
 
-console.log(playerSelection)
-console.log(computerSelection)
-//let playerSelection = getPlayerChoice()
 //Step 2 - Create a function to play a round of rock, paper, scissors
-
-//note - always returns "This round is a Draw"...need to fix the if then code//
-//note - consider using switches for the different cases//
-
+//note - add a try except functionality to require input to match rock, paper, or scissors
 function playRound (playerSelection, computerSelection){   
     const player = playerSelection
     const computer = computerSelection
     if (player === computer) {
-        result = "This round is a draw."
+        result = `This round is a draw. ${player.charAt(0).toUpperCase()}${player.slice(1)} matches ${computer}.`
     } else if ((player === "rock" && computer === "paper")||
         (player === "paper" && computer ==="scissors")||
         (player==="scissors"&& computer==="rock")){
-        result = "You lost this round."
+        result = `You lost this round. ${computer.charAt(0).toUpperCase()}${computer.slice(1)} beats ${player}!`
     } else if ((player === "rock" && computer === "scissors")||
         (player==="paper"&&computer==="rock")||
         (player==="scissors"&&computer==="paper")) {
-        result = "You won this round."
+        result = `You won this round.  ${player.charAt(0).toUpperCase()}${player.slice(1)} beats ${computer}!`
     } else {
         result = "Undefined"
     }
+    return result
 }
 
 //code for adding values to an array//
@@ -54,6 +49,8 @@ myArray.push('and oranges');
 //console.log(madeAString) => ['I', 'love', 'chocolate', 'frogs', 'and oranges']
 console.log(myArray)
 */
+
+
 
 //Console testing//
 console.log(playerSelection)
