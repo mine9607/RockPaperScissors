c0550f615bb663e4e7e2adba0ea7b2d26bc79edc
mine9607 <81383389+mine9607@users.noreply.github.com>
2022-12-05 20:16:49 -0500

    Added html, css and js files and wrote initial js code

---
+++ script.js
@@ -0,0 +1,43 @@
+//Step 1 - Create a function to have computer choose rock, paper, scissors//
+
+function getComputerChoice () {
+    let choice = ""
+    let computerResult = Math.floor(Math.random()*3);
+    if (computerResult === 0){
+        choice = "rock";
+    } else if (computerResult === 1){
+        choice = "paper";
+    } else {
+        choice = "scissors";
+    }
+    return choice;
+};
+
+let computerSelection = getComputerChoice()
+
+//Step 2a - Create a prompt for user input which stores playerSelection (make case insensitive)
+let playerSelection = prompt('Choose:  "Rock", "Paper" or "Scissors"').toLowerCase()
+
+//Step 2 - Create a function to play a round of rock, paper, scissors
+function playRound (playerSelection, getComputerChoice()){   
+    if (playerSelection === computerSelection) {
+        result = "This round is a Draw.";
+    } else if (playerSelection === "rock" && computerSelection === "scissors" || 
+        playerSelection === "paper" && computerSelection === "rock" ||
+        playerSelection === "scissors" && computerSelection === "paper") {
+        result = "You won this round.";
+    } else {
+        result = "You lost this round.";
+    }
+    return result;
+}
+
+
+
+//Console testing//
+console.log(playerSelection)
+console.log (getComputerChoice())
+console.log(playRound())
+
+
+
